82ae34bcfee999de9ccfd68ba5550d5c4e0c020d
SokhnaDior <sokhnadiorseck6@gmail.com>
2024-03-18 14:24:33 -0400

    deuxieme commit

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -3,10 +3,20 @@ import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 import CartItem from "@/app/(routes)/cart/components/cart-item";
 import Summary from "@/app/(routes)/cart/components/summary";
+import {useEffect, useState} from "react";
 
 const CartPage = () => {
 
+    const [isMounted, setIsMounted] = useState(false);
     const cart = useCart();
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+
+    if (!isMounted) {
+        return null;
+    }
 
   return (
       <div className="bg-white">
